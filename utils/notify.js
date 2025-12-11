47a4fc943c63ce0fc55e2967980435988dddacac
Kunal200317 <kunal200317@gmail.com>
2025-12-12 02:12:10 +0530

    gmail

--- utils/notify.js
+++ utils/notify.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail", 
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
