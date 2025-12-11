652b2799069bf10ab945e4ab2648bc56cf31c991
Kunal200317 <kunal200317@gmail.com>
2025-12-12 01:45:50 +0530

    email update

--- utils/notify.js
+++ utils/notify.js
@@ -2,16 +2,18 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  service: "gmail", 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000, 
+  socketTimeout: 45000,     
+  tls: {
+    rejectUnauthorized: false,
+    ciphers: 'SSLv3'
+  }
 });
 
 export const sendEmail = async ({ to, subject, html }) => {
